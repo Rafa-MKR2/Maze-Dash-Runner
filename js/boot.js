a83056f8b171d2d17df81cac85cd47920bc991a1
Rafael do Carmo <rafaeldocarmo2010@gmail.com>
2026-07-24 00:53:38 -0300

    feat(menu): restore and polish main menu
    - add settings menu with audio controls
    - improve menu animations
    - add retro chase animation
    - fix menu interaction flow
    - improve audio state handling
    - scale menu layout for larger display
    - refine typography and visual spacing
    - preserve original Phaser 2 implementation

--- js/boot.js
+++ js/boot.js
@@ -5,6 +5,13 @@ var bootState = {
 	},
 
 	create: function(){
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		game.scale.maxWidth = window.innerWidth - 60;
+		game.scale.maxHeight = window.innerHeight - 60;
+		game.scale.pageAlignHorizontally = true;
+		game.scale.pageAlignVertically = true;
+		game.antialias = false;
+
 		game.state.start('load');
 	}
 
--- js/menu.js
+++ js/menu.js
@@ -26,35 +26,45 @@ var menuState = {
 
 		// titulo - entra descendo
 		this.title = game.add.text(game.world.centerX, -50, 'MAZE DASH RUNNER', {
-			font: '40px emulogic', fill: '#fff'
+			font: '36px emulogic', fill: '#fff'
 		});
 		this.title.anchor.set(.5);
-		game.add.tween(this.title).to({y: 150}, 800, Phaser.Easing.Quadratic.Out, true);
+		game.add.tween(this.title).to({y: 80}, 800, Phaser.Easing.Quadratic.Out, true);
 
-		// press enter - entra subindo
-		this.txtPressStart = game.add.text(game.world.centerX, 550, 'PRESS ENTER', {
+		// enter the maze - entra subindo
+		this.txtPressStart = game.add.text(game.world.centerX, 550, 'ENTER THE MAZE', {
 			font: '20px emulogic', fill: '#fff'
 		});
 		this.txtPressStart.anchor.set(.5);
-		game.add.tween(this.txtPressStart).to({y: 250}, 800, Phaser.Easing.Quadratic.Out, true, 200);
+		var tweenPressStart = game.add.tween(this.txtPressStart).to({y: 270}, 800, Phaser.Easing.Quadratic.Out, true, 200);
 
-		// settings - entra subindo
+		// settings - entra subindo (ultima tween)
 		this.txtSettings = game.add.text(game.world.centerX, 600, 'SETTINGS', {
 			font: '20px emulogic', fill: '#fff'
 		});
 		this.txtSettings.anchor.set(.5);
-		game.add.tween(this.txtSettings).to({y: 320}, 800, Phaser.Easing.Quadratic.Out, true, 400);
+		var tweenSettings = game.add.tween(this.txtSettings).to({y: 330}, 800, Phaser.Easing.Quadratic.Out, true, 400);
 
-		// indicador de selecao
+		// indicador de selecao (criado invisivel)
 		this.arrow = game.add.text(0, 0, '>', {
 			font: '20px emulogic', fill: '#fff'
 		});
-		this.updateArrowPosition();
+		this.arrow.visible = false;
+
+		// habilitar input somente apos a ultima tween
+		tweenSettings.onComplete.add(function(){
+			this.inputReady = true;
+			this.arrow.visible = true;
+			this.updateArrowPosition();
+		}, this);
+
+		// efeito de blink no "ENTER THE MAZE"
+		this.blinkTween = game.add.tween(this.txtPressStart).to({alpha: 0.3}, 300, Phaser.Easing.Linear.None, true, 0, -1, true);
 
 		// --- elementos do settings (inicialmente ocultos) ---
 
-		this.settingsTitle = game.add.text(game.world.centerX, 100, 'SETTINGS', {
-			font: '40px emulogic', fill: '#fff'
+		this.settingsTitle = game.add.text(game.world.centerX, 80, 'SETTINGS', {
+			font: '36px emulogic', fill: '#fff'
 		});
 		this.settingsTitle.anchor.set(.5);
 		this.settingsTitle.visible = false;
@@ -87,9 +97,14 @@ var menuState = {
 		this.enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 		this.escKey = game.input.keyboard.addKey(Phaser.Keyboard.ESC);
 
-		// entrada retardada
-		game.time.events.add(1000, function(){
-			this.inputReady = true;
+		// garantir que audio funcione apos interacao do usuario
+		game.input.onDown.addOnce(function(){
+			if(game.sound.context && game.sound.context.state === 'suspended'){
+				game.sound.context.resume();
+			}
+			if(this.settings.music && this.menuMusic && !this.menuMusic.isPlaying){
+				this.menuMusic.play();
+			}
 		}, this);
 	},
 
@@ -109,17 +124,20 @@ var menuState = {
 		if(this.cursors.up.isDown && !this.cursors.down.isDown){
 			this.selectedIndex = 0;
 			this.updateArrowPosition();
+			this.playTick();
 			this.inputReady = false;
 			game.time.events.add(200, function(){ this.inputReady = true; }, this);
 		} else
 		if(this.cursors.down.isDown && !this.cursors.up.isDown){
 			this.selectedIndex = 1;
 			this.updateArrowPosition();
+			this.playTick();
 			this.inputReady = false;
 			game.time.events.add(200, function(){ this.inputReady = true; }, this);
 		}
 
 		if(this.enterKey.isDown){
+			this.flashSelected();
 			if(this.selectedIndex === 0){
 				this.startGame();
 			} else {
@@ -134,50 +152,64 @@ var menuState = {
 		if(this.cursors.up.isDown && !this.cursors.down.isDown){
 			this.settingsIndex = (this.settingsIndex + 2) % 3;
 			this.updateSettingsArrow();
+			this.playTick();
 			this.inputReady = false;
 			game.time.events.add(200, function(){ this.inputReady = true; }, this);
 		} else
 		if(this.cursors.down.isDown && !this.cursors.up.isDown){
 			this.settingsIndex = (this.settingsIndex + 1) % 3;
 			this.updateSettingsArrow();
+			this.playTick();
 			this.inputReady = false;
 			game.time.events.add(200, function(){ this.inputReady = true; }, this);
 		}
 
 		if(this.settingsIndex === 0){
 			if(this.cursors.left.isDown){
-				this.settings.music = false;
-				this.updateSettingsText();
-				this.saveSettings();
+				if(this.settings.music){
+					this.settings.music = false;
+					this.updateSettingsText();
+					this.saveSettings();
+					this.applyMusicSettings();
+				}
 				this.inputReady = false;
 				game.time.events.add(200, function(){ this.inputReady = true; }, this);
 			} else
 			if(this.cursors.right.isDown){
-				this.settings.music = true;
-				this.updateSettingsText();
-				this.saveSettings();
+				if(!this.settings.music){
+					this.settings.music = true;
+					this.updateSettingsText();
+					this.saveSettings();
+					this.applyMusicSettings();
+				}
 				this.inputReady = false;
 				game.time.events.add(200, function(){ this.inputReady = true; }, this);
 			}
 		} else
 		if(this.settingsIndex === 1){
 			if(this.cursors.left.isDown){
-				this.settings.sfx = false;
-				this.updateSettingsText();
-				this.saveSettings();
+				if(this.settings.sfx){
+					this.settings.sfx = false;
+					this.updateSettingsText();
+					this.saveSettings();
+				}
 				this.inputReady = false;
 				game.time.events.add(200, function(){ this.inputReady = true; }, this);
 			} else
 			if(this.cursors.right.isDown){
-				this.settings.sfx = true;
-				this.updateSettingsText();
-				this.saveSettings();
+				if(!this.settings.sfx){
+					this.settings.sfx = true;
+					this.updateSettingsText();
+					this.saveSettings();
+					this.applySfxFeedback();
+				}
 				this.inputReady = false;
 				game.time.events.add(200, function(){ this.inputReady = true; }, this);
 			}
 		}
 
 		if(this.enterKey.isDown && this.settingsIndex === 2){
+			this.flashSelectedSettings();
 			this.closeSettings();
 			this.inputReady = false;
 			game.time.events.add(500, function(){ this.inputReady = true; }, this);
@@ -191,13 +223,23 @@ var menuState = {
 	},
 
 	updateArrowPosition: function(){
-		var y = this.selectedIndex === 0 ? 250 : 320;
-		this.arrow.x = game.world.centerX - 110;
-		this.arrow.y = y;
+		var y = this.selectedIndex === 0 ? 270 : 330;
+		var target = this.selectedIndex === 0 ? this.txtPressStart : this.txtSettings;
+		this.arrow.x = game.world.centerX - target.width / 2 - 20;
+		this.arrow.y = y - 12;
+
+		// blink so no item selecionado
+		if(this.selectedIndex === 0){
+			this.txtPressStart.alpha = 1;
+			if(!this.blinkTween.isRunning) this.blinkTween.start();
+		} else {
+			this.blinkTween.stop();
+			this.txtPressStart.alpha = 1;
+		}
 	},
 
 	updateSettingsArrow: function(){
-		var positions = [200, 260, 340];
+		var positions = [190, 250, 330];
 		this.settingsArrow.x = game.world.centerX - 130;
 		this.settingsArrow.y = positions[this.settingsIndex];
 	},
@@ -211,6 +253,8 @@ var menuState = {
 		this.settingsMode = true;
 		this.settingsIndex = 0;
 
+		this.blinkTween.stop();
+		this.txtPressStart.alpha = 1;
 		this.title.visible = false;
 		this.txtPressStart.visible = false;
 		this.txtSettings.visible = false;
@@ -244,43 +288,79 @@ var menuState = {
 	},
 
 	createChaseAnimation: function(){
-		// moeda
-		this.chaseCoin = game.add.sprite(-30, 430, 'coin');
+		this.chaseGap = 60;
+		this.chaseSpeed = 180;
+		this.chaseTrack = game.width + 60 + this.chaseGap * 2;
+
+		this.chaseCoin = game.add.sprite(-30, 165, 'coin');
 		this.chaseCoin.anchor.set(.5);
-		this.chaseCoin.scale.set(0.6);
+		this.chaseCoin.smoothed = false;
 		this.chaseCoin.animations.add('spin', [0,1,2,3,4,5,6,7,8,9], 10, true).play();
-		this.chaseCoinSpeed = 40;
 
-		// personagem
-		this.chasePlayer = game.add.sprite(-80, 430, 'player');
+		this.chasePlayer = game.add.sprite(-30 - this.chaseGap, 165, 'player');
 		this.chasePlayer.anchor.set(.5);
-		this.chasePlayer.scale.set(0.6);
+		this.chasePlayer.smoothed = false;
 		this.chasePlayer.animations.add('run', [24,25,26,27,28,29,30,31], 12, true).play();
-		this.chasePlayerSpeed = 60;
 
-		// inimigo
-		this.chaseEnemy = game.add.sprite(-130, 430, 'enemy');
+		this.chaseEnemy = game.add.sprite(-30 - this.chaseGap * 2, 165, 'enemy');
 		this.chaseEnemy.anchor.set(.5);
-		this.chaseEnemy.scale.set(0.6);
+		this.chaseEnemy.smoothed = false;
 		this.chaseEnemy.animations.add('run', [24,25,26,27,28,29,30,31], 12, true).play();
-		this.chaseEnemySpeed = 80;
 	},
 
 	updateChaseAnimation: function(){
-		this.chaseCoin.x += this.chaseCoinSpeed * game.time.physicsElapsed;
+		var speed = this.chaseSpeed * game.time.physicsElapsed;
+
+		this.chaseCoin.x += speed;
+		this.chasePlayer.x += speed;
+		this.chaseEnemy.x += speed;
+
 		if(this.chaseCoin.x > game.width + 30){
-			this.chaseCoin.x = -30;
+			this.chaseCoin.x -= this.chaseTrack;
 		}
-
-		this.chasePlayer.x += this.chasePlayerSpeed * game.time.physicsElapsed;
 		if(this.chasePlayer.x > game.width + 30){
-			this.chasePlayer.x = -30;
+			this.chasePlayer.x -= this.chaseTrack;
 		}
-
-		this.chaseEnemy.x += this.chaseEnemySpeed * game.time.physicsElapsed;
 		if(this.chaseEnemy.x > game.width + 30){
-			this.chaseEnemy.x = -30;
+			this.chaseEnemy.x -= this.chaseTrack;
 		}
+	},
+
+	playTick: function(){
+		if(!this.settings.sfx) return;
+		var tick = game.add.audio('getitem');
+		tick.volume = 0.2;
+		tick.play();
+	},
+
+	flashSelected: function(){
+		var target = this.selectedIndex === 0 ? this.txtPressStart : this.txtSettings;
+		game.add.tween(target).to({alpha: 0.3}, 50, Phaser.Easing.Linear.None, true, 0, 0, true);
+	},
+
+	flashSelectedSettings: function(){
+		game.add.tween(this.txtBack).to({alpha: 0.3}, 50, Phaser.Easing.Linear.None, true, 0, 0, true);
+	},
+
+	applyMusicSettings: function(){
+		if(this.settings.music){
+			if(!this.menuMusic){
+				this.menuMusic = game.add.audio('music');
+				this.menuMusic.loop = true;
+				this.menuMusic.volume = .5;
+			}
+			this.menuMusic.play();
+		} else {
+			if(this.menuMusic){
+				this.menuMusic.stop();
+			}
+		}
+	},
+
+	applySfxFeedback: function(){
+		var snd = game.add.audio('getitem');
+		snd.volume = .5;
+		snd.play();
 	},
 
 	shutdown: function(){
